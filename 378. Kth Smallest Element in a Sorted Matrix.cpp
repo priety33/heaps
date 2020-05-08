1bd0adfa5f4442b2a3c335e56cd0b7d286110325
priety33 <52635177+priety33@users.noreply.github.com>
2020-05-08 16:23:10 +0530

    Update 378. Kth Smallest Element in a Sorted Matrix.cpp

--- 378. Kth Smallest Element in a Sorted Matrix.cpp
+++ 378. Kth Smallest Element in a Sorted Matrix.cpp
@@ -60,3 +60,48 @@ public:
     return t[k-1];
     }
 };
+
+//using binary search
+//finding middle element of matrix- then counting in every row, number of elements <= mid
+//if that count<k => increment low, else decrement high
+class Solution {
+public:
+   
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        int low=matrix[0][0];
+        int high=matrix[n-1][n-1];
+        int counts=0;
+        
+        while(low<high){
+            int mid=(low+high)/2;
+            counts=0;
+            for(int i=0;i<n;i++){
+                int col=binsearch(matrix[i],mid);
+                counts+=(col+1); //count of elements<=mid in current row
+            }
+            
+            if(counts<k)
+                low=mid+1;
+            else
+                high=mid;
+            
+        }
+        return low;
+    }
+    
+    int binsearch(vector<int> &matrix,int target){
+        if(matrix[0]>target)
+            return -1;
+        int low=0;
+        int high=matrix.size()-1;
+        while(low<high){
+            int mid=(low+high+1)/2;
+            if(matrix[mid]<=target)
+                low=mid;
+            else
+                high=mid-1;
+        }
+        return low;
+    }
+};
